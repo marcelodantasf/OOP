f6d759055b69f409d67d24ad73b37fa77591bae4
larissasantiagoa <lalasalbino@gmail.com>
2024-03-18 22:41:15 -0300

    feat: metodos get e set implementados

--- TrainStation/Estacao.java
+++ TrainStation/Estacao.java
@@ -3,4 +3,30 @@ public class Estacao{
 uma sigla e uma descrição. */
 private String sigla;
 private String desc;
+
+
+public void setSigla(String sigla){
+    this.sigla = sigla;
 }
+
+public String getSigla(){
+    return sigla;
+}
+
+public void setDesc(String desc){
+    this.desc = desc;
+}
+
+public String getDesc(){
+    return desc;
+}
+
+public String toString(){
+    String str = "";
+    str += "Sigla: " + this.sigla + 
+    "\nDescrição: " + this.desc;
+    return str;
+}
+
+}
+
--- TrainStation/Locomotiva.java
+++ TrainStation/Locomotiva.java
@@ -1,7 +1,41 @@
 public class Locomotiva{
     /* Uma locomotiva é descrita por um número de série, capacidade de tração e comprimento,
 também podendo ser opcionalmente informados durante a sua criação.*/
-    private int serial;
+    private String serial;
     private float comprimento;
     private float tracao;
+
+    public void setSerial(String serial){
+        this.serial = serial;
+    }
+    
+    public String getSerial(){
+        return serial;
+    }
+    
+    public void setComprimento(float comprimento){
+        this.comprimento = comprimento;
+    }
+    
+    public float getComprimento(){
+        return comprimento;
+    }
+    
+    public void setTracao(float tracao){
+        this.tracao = tracao;
+    }
+    
+    public float getTracao(){
+        return tracao;
+    }
+    
+    public String toString(){
+        String str = "";
+        str += "Serial: " + this.serial + 
+        "\nComprimento: " + this.comprimento + 
+        "\nTração: " + this.tracao;
+        return str;
+    }
+    
 }
+
